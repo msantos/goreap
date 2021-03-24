fe7f36626e5035208962a5b7a818a11ffdb24135
Michael Santos <michael.santos@gmail.com>
2021-03-24 07:44:54 -0400

    tests: fix test, add test for ReadProcList()

--- process/process_test.go
+++ process/process_test.go
@@ -12,9 +12,11 @@ func TestNew(t *testing.T) {
 	ps, err := process.New()
 	if err != nil {
 		t.Errorf("%v", err)
+		return
 	}
 	if pid := os.Getpid(); pid != ps.Pid {
 		t.Errorf("pid = %d, want %d", ps.Pid, pid)
+		return
 	}
 }
 
@@ -22,12 +24,37 @@ func TestNewWithProcfs(t *testing.T) {
 	procfs := "/bin"
 	if err := os.Setenv("PROC", procfs); err != nil {
 		t.Errorf("%v", err)
+		return
 	}
 	_, err := process.New()
 	if err == nil {
 		t.Errorf("non-existent procfs %s", procfs)
+		return
 	}
 	if !errors.Is(err, process.ErrProcNotMounted) {
 		t.Errorf("procfs error = %v, want %v", err, process.ErrProcNotMounted)
+		return
+	}
+	if err := os.Unsetenv("PROC"); err != nil {
+		t.Errorf("%v", err)
+		return
+	}
+}
+
+func TestReadProcList(t *testing.T) {
+	ps, err := process.New()
+	if err != nil {
+		t.Errorf("%v", err)
+		return
+	}
+	ps.Pid = 1
+	pids, err := ps.ReadProcList()
+	if err != nil {
+		t.Errorf("%v", err)
+		return
+	}
+	if len(pids) == 0 {
+		t.Errorf("process table is empty = %v", ps)
+		return
 	}
 }
