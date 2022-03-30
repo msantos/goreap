3360b918fbce4b28919a7bb93aaa3c1cfc1b2354
Michael Santos <michael.santos@gmail.com>
2022-03-30 07:48:40 -0400

    cleanup: move process reaping to package
    Move the subreaper out of the command and into a package to make testing
    easier. Note using the reap package as a library has some complications.
    
    According to prctl(2):
    
    * PR_SET_CHILD_SUBREAPER applies to the process
    
            PR_SET_CHILD_SUBREAPER (since Linux 3.4)
    
                  If arg2 is nonzero, set the "child subreaper" attribute of
                  the calling process; if arg2 is zero, unset the attribute.
    
    * PR_SET_NO_NEW_PRIVS applies to the calling thread
    
            PR_SET_NO_NEW_PRIVS (since Linux 3.5)
    
                  Set the calling thread's no_new_privs attribute to the
                  value in arg2.
    
    The consequence is:
    
    * the caller may call PR_SET_CHILD_SUBREAPER multiple times
    
      From testing, subsequent calls to prctl(PR_SET_CHILD_SUBREAPER)
      appears to be a noop.
    
    * PR_SET_CHILD_SUBREAPER should affect all threads and goroutines scheduled on
      those threads including threads created before the call to
      prctl(PR_SET_CHILD_SUBREAPER)
    
    * PR_SET_NO_NEW_PRIVS will affect the current thread and will not change
      the privilege status of threads created before calling
      prctl(PR_SET_NO_NEW_PRIVS)
    
    * the caller may call PR_SET_CHILD_SUBREAPER multiple times
    
      The behaviour may or may not change depending on whether the prctl()
      operation had been previously called on this thread. Calling
      PR_SET_CHILD_SUBREAPER on the same thread is a noop.
    
    * the Go scheduler may (reschedule a goroutine on a different thread
      after a syscall)[https://golang.design/research/zero-alloc-call-sched/]
    
      It may be possible that a goroutine executing a binary will be
      rescheduled from a thread with setuid disabled to a thread that does
      not have setuid disabled.

--- cmd/goreap/main.go
+++ cmd/goreap/main.go
@@ -1,38 +1,26 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	"fmt"
 	"os"
-	"os/exec"
-	"os/signal"
 	"path"
-	"syscall"
 	"time"
 
-	"github.com/msantos/goreap/process"
-
-	"golang.org/x/sys/unix"
+	"github.com/msantos/goreap/reap"
 )
 
 var version = "0.8.0"
 
 type stateT struct {
 	argv          []string
-	sig           syscall.Signal
+	sig           int
 	disableSetuid bool
 	wait          bool
 	verbose       bool
 	deadline      time.Duration
 	delay         time.Duration
-	ps            process.Process
-	sigChan       chan os.Signal
 }
-
-const (
-	maxInt64 = 1<<63 - 1
-)
 
 func args() *stateT {
 	flag.Usage = func() {
@@ -44,8 +32,7 @@ Options:
 		flag.PrintDefaults()
 	}
 
-	sig := flag.Int("signal", int(syscall.SIGTERM),
-		"signal sent to supervised processes")
+	sig := flag.Int("signal", 15, "signal sent to supervised processes")
 	disableSetuid := flag.Bool("disable-setuid", false,
 		"disallow setuid (unkillable) subprocesses")
 	wait := flag.Bool("wait", false, "wait for subprocesses to exit")
@@ -68,169 +55,45 @@ Options:
 		os.Exit(1)
 	}
 
-	ps, err := process.New()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(111)
-	}
-
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan)
-
 	return &stateT{
 		argv:          flag.Args(),
-		sig:           syscall.Signal(*sig),
+		sig:           *sig,
 		disableSetuid: *disableSetuid,
 		wait:          *wait,
 		deadline:      *deadline,
 		delay:         *delay,
 		verbose:       *verbose,
-		ps:            ps,
-		sigChan:       sigChan,
 	}
 }
 
 func main() {
 	state := args()
-	if state.disableSetuid {
-		if err := unix.Prctl(unix.PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0); err != nil {
-			fmt.Fprintf(os.Stderr, "prctl(PR_SET_NO_NEW_PRIVS): %s\n", err)
-			os.Exit(111)
+
+	var l func(error)
+
+	if state.verbose {
+		l = func(err error) {
+			fmt.Println(err)
 		}
 	}
-	if err := unix.Prctl(unix.PR_SET_CHILD_SUBREAPER, 1, 0, 0, 0); err != nil {
-		fmt.Fprintf(os.Stderr, "prctl(PR_SET_CHILD_SUBREAPER): %s\n", err)
+
+	r, err := reap.New(
+		reap.SetDeadline(state.deadline),
+		reap.SetDelay(state.delay),
+		reap.SetDisableSetuid(state.disableSetuid),
+		reap.SetSignal(state.sig),
+		reap.SetWait(state.wait),
+		reap.SetLog(l),
+	)
+	if err != nil {
+		fmt.Println(err)
 		os.Exit(111)
 	}
-	exitStatus := state.execv(state.argv[0], state.argv[1:], os.Environ())
-	if err := state.reap(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-	}
-	os.Exit(exitStatus)
-}
 
-func kill(pid int, sig syscall.Signal) {
-	err := syscall.Kill(pid, sig)
-	if err == nil || errors.Is(err, syscall.ESRCH) {
-		return
-	}
-	fmt.Fprintln(os.Stderr, err)
-}
-
-func (state *stateT) signalWith(sig syscall.Signal) {
-	pids, err := state.ps.Children()
+	status, err := r.Exec(state.argv, os.Environ())
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		return
+		fmt.Printf("%s: %s\n", state.argv[0], err)
 	}
 
-	for _, pid := range pids {
-		if state.verbose {
-			fmt.Fprintf(os.Stderr, "%d: kill %d %d\n", state.ps.Pid(), sig, pid)
-		}
-
-		kill(pid, sig)
-	}
-}
-
-func (state *stateT) reap() error {
-	go func() {
-		deadline := state.deadline
-		if deadline <= 0 {
-			deadline = time.Duration(maxInt64)
-		}
-
-		t := time.NewTimer(deadline)
-		tick := time.NewTicker(state.delay)
-
-		sig := state.sig
-
-		if !state.wait {
-			state.signalWith(sig)
-		}
-
-		for {
-			select {
-			case <-t.C:
-				sig = syscall.SIGKILL
-			case sig := <-state.sigChan:
-				switch sig.(syscall.Signal) {
-				case syscall.SIGCHLD, syscall.SIGIO, syscall.SIGPIPE, syscall.SIGURG:
-				default:
-					state.signalWith(sig.(syscall.Signal))
-				}
-			case <-tick.C:
-				if !state.wait {
-					state.signalWith(sig)
-				}
-			}
-		}
-	}()
-
-	for {
-		_, err := syscall.Wait4(-1, nil, 0, nil)
-		switch {
-		case err == nil, errors.Is(err, syscall.EINTR):
-		case errors.Is(err, syscall.ECHILD):
-			return nil
-		default:
-			return err
-		}
-	}
-}
-
-func (state *stateT) execv(command string, args []string, env []string) int {
-	cmd := exec.Command(command, args...)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Env = env
-
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Pdeathsig: syscall.SIGKILL,
-	}
-
-	if err := cmd.Start(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		return 127
-	}
-	waitCh := make(chan error, 1)
-	go func() {
-		waitCh <- cmd.Wait()
-		close(waitCh)
-	}()
-
-	var exitError *exec.ExitError
-
-	for {
-		select {
-		case sig := <-state.sigChan:
-			switch sig.(syscall.Signal) {
-			case syscall.SIGCHLD, syscall.SIGIO, syscall.SIGPIPE, syscall.SIGURG:
-			default:
-				state.signalWith(sig.(syscall.Signal))
-			}
-		case err := <-waitCh:
-			if err == nil {
-				return 0
-			}
-
-			if !errors.As(err, &exitError) {
-				fmt.Fprintln(os.Stderr, err)
-				return 111
-			}
-
-			waitStatus, ok := exitError.Sys().(syscall.WaitStatus)
-			if !ok {
-				fmt.Fprintln(os.Stderr, err)
-				return 111
-			}
-
-			if waitStatus.Signaled() {
-				return 128 + int(waitStatus.Signal())
-			}
-
-			return waitStatus.ExitStatus()
-		}
-	}
+	os.Exit(status)
 }
---
+++ reap/reap.go
@@ -0,0 +1,236 @@
+package reap
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/msantos/goreap/process"
+
+	"golang.org/x/sys/unix"
+)
+
+const (
+	maxInt64 = 1<<63 - 1
+)
+
+type Reap struct {
+	sig           syscall.Signal
+	disableSetuid bool
+	wait          bool
+	deadline      time.Duration
+	delay         time.Duration
+	log           func(error)
+
+	ps    process.Process
+	sigch chan os.Signal
+}
+
+type ReapOption func(*Reap)
+
+func SetDeadline(t time.Duration) ReapOption {
+	return func(r *Reap) {
+		r.deadline = t
+	}
+}
+
+func SetDelay(t time.Duration) ReapOption {
+	return func(r *Reap) {
+		r.delay = t
+	}
+}
+
+func SetDisableSetuid(b bool) ReapOption {
+	return func(r *Reap) {
+		r.disableSetuid = b
+	}
+}
+
+func SetLog(f func(error)) ReapOption {
+	return func(r *Reap) {
+		r.log = f
+	}
+}
+
+func SetSignal(sig int) ReapOption {
+	return func(r *Reap) {
+		r.sig = syscall.Signal(sig)
+	}
+}
+
+func SetWait(b bool) ReapOption {
+	return func(r *Reap) {
+		r.wait = b
+	}
+}
+
+func New(opts ...ReapOption) (*Reap, error) {
+	r := &Reap{}
+
+	ps, err := process.New()
+	if err != nil {
+		return r, err
+	}
+
+	sigch := make(chan os.Signal, 1)
+	signal.Notify(sigch)
+
+	r.ps = ps
+	r.sigch = sigch
+
+	for _, opt := range opts {
+		opt(r)
+	}
+
+	if r.log == nil {
+		r.log = func(error) {}
+	}
+
+	if err := unix.Prctl(unix.PR_SET_CHILD_SUBREAPER, 1, 0, 0, 0); err != nil {
+		return r, fmt.Errorf("prctl(PR_SET_CHILD_SUBREAPER): %w", err)
+	}
+
+	return r, nil
+}
+
+func (r *Reap) Exec(argv []string, env []string) (int, error) {
+	if r.disableSetuid {
+		if err := unix.Prctl(unix.PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0); err != nil {
+			return 111, fmt.Errorf("prctl(PR_SET_NO_NEW_PRIVS): %w", err)
+		}
+	}
+
+	exitStatus := r.execv(argv[0], argv[1:], env)
+	if err := r.reap(); err != nil {
+		return 111, err
+	}
+
+	return exitStatus, nil
+}
+
+func (r *Reap) kill(pid int, sig syscall.Signal) {
+	err := syscall.Kill(pid, sig)
+	if err == nil || errors.Is(err, syscall.ESRCH) {
+		return
+	}
+	r.log(err)
+}
+
+func (r *Reap) signalWith(sig syscall.Signal) {
+	pids, err := r.ps.Children()
+	if err != nil {
+		r.log(err)
+		return
+	}
+
+	for _, pid := range pids {
+		r.log(fmt.Errorf("%d: kill %d %d", r.ps.Pid(), sig, pid))
+		r.kill(pid, sig)
+	}
+}
+
+func (r *Reap) reap() error {
+	go func() {
+		deadline := r.deadline
+		if deadline <= 0 {
+			deadline = time.Duration(maxInt64)
+		}
+
+		t := time.NewTimer(deadline)
+		tick := time.NewTicker(r.delay)
+
+		sig := r.sig
+
+		if !r.wait {
+			r.signalWith(sig)
+		}
+
+		for {
+			select {
+			case <-t.C:
+				sig = syscall.SIGKILL
+			case sig := <-r.sigch:
+				switch sig.(syscall.Signal) {
+				case syscall.SIGCHLD, syscall.SIGIO, syscall.SIGPIPE, syscall.SIGURG:
+				default:
+					r.signalWith(sig.(syscall.Signal))
+				}
+			case <-tick.C:
+				if !r.wait {
+					r.signalWith(sig)
+				}
+			}
+		}
+	}()
+
+	for {
+		_, err := syscall.Wait4(-1, nil, 0, nil)
+		switch {
+		case err == nil, errors.Is(err, syscall.EINTR):
+		case errors.Is(err, syscall.ECHILD):
+			return nil
+		default:
+			return err
+		}
+	}
+}
+
+func (r *Reap) execv(command string, args []string, env []string) int {
+	cmd := exec.Command(command, args...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Env = env
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Pdeathsig: syscall.SIGKILL,
+	}
+
+	if err := cmd.Start(); err != nil {
+		r.log(err)
+		return 127
+	}
+	waitCh := make(chan error, 1)
+	go func() {
+		waitCh <- cmd.Wait()
+		close(waitCh)
+	}()
+
+	var exitError *exec.ExitError
+
+	for {
+		select {
+		case sig := <-r.sigch:
+			switch sig.(syscall.Signal) {
+			case syscall.SIGCHLD, syscall.SIGIO, syscall.SIGPIPE, syscall.SIGURG:
+			default:
+				r.signalWith(sig.(syscall.Signal))
+			}
+		case err := <-waitCh:
+			if err == nil {
+				return 0
+			}
+
+			if !errors.As(err, &exitError) {
+				r.log(err)
+				return 111
+			}
+
+			waitStatus, ok := exitError.Sys().(syscall.WaitStatus)
+			if !ok {
+				r.log(err)
+				return 111
+			}
+
+			if waitStatus.Signaled() {
+				return 128 + int(waitStatus.Signal())
+			}
+
+			return waitStatus.ExitStatus()
+		}
+	}
+}
