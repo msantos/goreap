9635fa3b55ba5d4a1c08db17800197a9047d99e5
Michael Santos <michael.santos@gmail.com>
2022-04-04 07:34:29 -0400

    test: add tests for reap

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/msantos/goreap
 
 go 1.15
 
-require golang.org/x/sys v0.0.0-20210113181707-4bcb84eeeb78
+require (
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
+	golang.org/x/sys v0.0.0-20210113181707-4bcb84eeeb78
+)
---
+++ reap/reap_test.go
@@ -0,0 +1,172 @@
+package reap_test
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"runtime"
+	"testing"
+	"time"
+	"unsafe"
+
+	"github.com/msantos/goreap/process"
+	"github.com/msantos/goreap/reap"
+	"golang.org/x/sync/errgroup"
+	"golang.org/x/sys/unix"
+)
+
+var (
+	errNotSubreaper  = errors.New("process is not subreaper")
+	errSetuidEnabled = errors.New("process does not disable setuid")
+	errReapFailed    = errors.New("process failed to reap subprocesses")
+)
+
+func TestNew(t *testing.T) {
+	_, err := reap.New(
+		reap.SetLog(func(err error) {
+			t.Log(err)
+		}),
+	)
+	if err != nil {
+		t.Errorf("%v", err)
+		return
+	}
+
+	g := new(errgroup.Group)
+	n := runtime.NumCPU() * 2
+
+	for i := n; i > 0; i-- {
+		g.Go(func() error {
+			var v uintptr
+			if err := unix.Prctl(unix.PR_GET_CHILD_SUBREAPER, uintptr(unsafe.Pointer(&v)), 0, 0, 0); err != nil {
+				return err
+			}
+			if int(v) != 0 {
+				return nil
+			}
+			return fmt.Errorf("%d: %w", int(v), errNotSubreaper)
+		})
+	}
+
+	if err := g.Wait(); err != nil {
+		t.Errorf("%v", err)
+	}
+}
+
+func TestDisableSetuid(t *testing.T) {
+	r, err := reap.New(
+		reap.SetDisableSetuid(true),
+		reap.SetLog(func(err error) {
+			t.Log(err)
+		}),
+	)
+	if err != nil {
+		t.Errorf("%v", err)
+		return
+	}
+
+	g := new(errgroup.Group)
+	n := runtime.NumCPU() * 2
+
+	for i := n; i > 0; i-- {
+		g.Go(func() error {
+			status, err := r.Exec([]string{"sh", "-c", "sudo -h 2>/dev/null"}, os.Environ())
+			if err != nil {
+				return err
+			}
+			if status == 0 {
+				return fmt.Errorf("sudo: %d: %w", status, errSetuidEnabled)
+			}
+			v, err := unix.PrctlRetInt(unix.PR_GET_NO_NEW_PRIVS, 0, 0, 0, 0)
+			if err != nil {
+				return err
+			}
+			if v == 1 {
+				return nil
+			}
+			return fmt.Errorf("prctl: %d: %w", v, errSetuidEnabled)
+		})
+	}
+
+	if err := g.Wait(); err != nil {
+		t.Errorf("%v", err)
+	}
+}
+
+func exec(r *reap.Reap, cmd []string, n int) error {
+	g := new(errgroup.Group)
+
+	for i := n; i > 0; i-- {
+		g.Go(func() error {
+			_, err := r.Exec(cmd, os.Environ())
+			if err != nil {
+				return err
+			}
+			return nil
+		})
+	}
+
+	if err := g.Wait(); err != nil {
+		return err
+	}
+
+	ps, err := process.New()
+	if err != nil {
+		return err
+	}
+
+	children, err := ps.Children()
+	if err != nil {
+		return err
+	}
+
+	if len(children) != 0 {
+		return errReapFailed
+	}
+
+	return nil
+}
+
+func TestExec(t *testing.T) {
+	r, err := reap.New(
+		reap.SetLog(func(err error) {
+			t.Log(err)
+		}),
+	)
+	if err != nil {
+		t.Errorf("%v", err)
+		return
+	}
+
+	cmd := []string{
+		"bash", "-c",
+		"(exec -a goreaptest-exec sleep 120) & (exec -a goreaptest-exec sleep 120) & (exec -a goreaptest-exec sleep 120) &",
+	}
+
+	if err := exec(r, cmd, 3); err != nil {
+		t.Errorf("%v", err)
+	}
+}
+
+func TestExecDeadline(t *testing.T) {
+	r, err := reap.New(
+		reap.SetSignal(15),
+		reap.SetDeadline(time.Duration(1)*time.Second),
+		reap.SetLog(func(err error) {
+			t.Log(err)
+		}),
+	)
+	if err != nil {
+		t.Errorf("%v", err)
+		return
+	}
+
+	cmd := []string{
+		"bash", "-c",
+		"trap '' TERM; (exec -a goreaptest-deadline sleep 120) & (exec -a goreaptest-deadline sleep 120) & (exec -a goreaptest-deadline sleep 120) &",
+	}
+
+	if err := exec(r, cmd, 3); err != nil {
+		t.Errorf("%v", err)
+	}
+}
